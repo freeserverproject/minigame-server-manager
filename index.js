b2fb1569a31e41d9f5e33b7e9c9a0cbad971ff35
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-03-11 15:19:35 +0900

    register events

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+require('dotenv').config();
+
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 require('discord.js-slash-command')(bot);
 
 const events = require('./events');
 
-bot
+bot.on('ready', events.ready);
+bot.on('interactionCreate', events.interactionCreate);
+
+bot.login(process.env.DISCORD_BOT_TOKEN);
