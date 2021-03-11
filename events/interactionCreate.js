44f977737d48c6d3b211c11afde09710471eb62e
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-03-11 15:28:24 +0900

    fixed

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -8,32 +8,34 @@ module.exports = function (interaction) {
 	const serverValue = options.find(a => a.name === 'サーバー名').value;
 	const server = servers.find(server => server.name === serverValue);
 
-	if (setting.allowUsers.includes(member.user.id)) {
-		child_process.exec(server.run, {
-			cwd: server.wd
-		}, function (error, stdout, stderr) {
-			if (error) {
-				this.api.interactions(interaction.id, interaction.token).callback.post({data: {
-					type: 4,
-					data: {
-						content: `${server.label}の起動に失敗しました`
-					}
-				}});
-			} else {
-				this.api.interactions(interaction.id, interaction.token).callback.post({data: {
-					type: 4,
-					data: {
-						content: `${server.label}を起動しました`
-					}
-				}});
-			}
-		});
-	} else {
-		this.api.interactions(interaction.id, interaction.token).callback.post({data: {
-			type: 4,
-			data: {
-				content: `${server.label}の起動に失敗しました`
-			}
-		}});
+	if (data.name === 'start') {
+		if (setting.allowUsers.includes(member.user.id)) {
+			child_process.exec(server.run, {
+				cwd: server.wd
+			}, function (error, stdout, stderr) {
+				if (error) {
+					this.api.interactions(interaction.id, interaction.token).callback.post({data: {
+						type: 4,
+						data: {
+							content: `${server.label}の起動に失敗しました`
+						}
+					}});
+				} else {
+					this.api.interactions(interaction.id, interaction.token).callback.post({data: {
+						type: 4,
+						data: {
+							content: `${server.label}を起動しました`
+						}
+					}});
+				}
+			});
+		} else {
+			this.api.interactions(interaction.id, interaction.token).callback.post({data: {
+				type: 4,
+				data: {
+					content: `${server.label}の起動に失敗しました`
+				}
+			}});
+		}
 	}
 }
