e2c4463c57093440cc2c30db664d0f49051deb74
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-03-11 15:20:26 +0900

    on interaction added

---
+++ events/interactionCreate.js
@@ -0,0 +1,39 @@
+const setting = require('../setting');
+const servers = require('../servers');
+const child_process = require('child_process');
+
+module.exports = function (interaction) {
+	const { member, data } = interaction;
+	const { options } = data;
+	const serverValue = options.find(a => a.name === 'サーバー名').value;
+	const server = servers.find(server => server.name === serverValue);
+
+	if (setting.allowUsers.includes(member.user.id)) {
+		child_process.exec(server.run, {
+			cwd: server.wd
+		}, function (error, stdout, stderr) {
+			if (error) {
+				this.api.interactions(interaction.id, interaction.token).callback.post({data: {
+					type: 4,
+					data: {
+						content: `${server.label}の起動に失敗しました`
+					}
+				}});
+			} else {
+				this.api.interactions(interaction.id, interaction.token).callback.post({data: {
+					type: 4,
+					data: {
+						content: `${server.label}を起動しました`
+					}
+				}});
+			}
+		});
+	} else {
+		this.api.interactions(interaction.id, interaction.token).callback.post({data: {
+			type: 4,
+			data: {
+				content: `${server.label}の起動に失敗しました`
+			}
+		}});
+	}
+}
