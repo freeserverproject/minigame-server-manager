bae6f24ddfdc7eff5e1e94785f16639e43530b10
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-03-11 15:20:47 +0900

    Register slash command

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,21 @@
+const setting = require('../setting');
+const servers = require('../servers');
 
 module.exports = function () {
-	
+	this.registerCommandToGuild(setting.targetGuildId, {
+		name: 'start',
+		description: 'ミニゲーム鯖を起動します',
+		options: [
+			{
+				name: 'サーバー名',
+				description: '起動したいサーバー名を指定してください',
+				type: 3,
+				required: true,
+				choices: servers.map(server => ({
+					name: server.label,
+					value: server.name
+				}))
+			}
+		]
+	});
 }
