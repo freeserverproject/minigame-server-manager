73ed16303a903a147d1857884a4f7c8b42e4a084
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-03-11 15:19:46 +0900

    added servers

--- servers.js
+++ servers.js
@@ -1,7 +1,48 @@
 module.exports = [
 	{
+		name: 'craftForest',
 		label: 'クラフトの森',
 		run: './start.sh',
 		wd: '/home/minigames/craftforest/'
+	}, {
+		name: 'hideandseek',
+		label: '動物かくれんぼ',
+		run: './start.sh',
+		wd: '/home/minigames/hideandseek/'
+	}, {
+		name: 'kinokogari',
+		label: 'きのこ狩り',
+		run: './start.sh',
+		wd: '/home/minigames/kinokogari/'
+	}, {
+		name: 'monodukuri',
+		label: 'ものづくり',
+		run: './start.sh',
+		wd: '/home/minigames/monodukuri/'
+	}, {
+		name: 'paintit',
+		label: 'Plaint it',
+		run: './start.sh',
+		wd: '/home/minigames/paintit/'
+	}, {
+		name: 'questers',
+		label: 'クエスターズ',
+		run: './start.sh',
+		wd: '/home/minigames/questers/'
+	}, {
+		name: 'snowballfight',
+		label: '雪合戦',
+		run: './start.sh',
+		wd: '/home/minigames/snowballfight/'
+	}, {
+		name: 'taggame',
+		label: '鬼ごっこ',
+		run: './start.sh',
+		wd: '/home/minigames/taggame/'
+	}, {
+		name: 'tct',
+		label: '人狼',
+		run: './start.sh',
+		wd: '/home/minigames/tct/'
 	}
 ]
